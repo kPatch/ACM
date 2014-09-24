6ecf02a606c67ea1fd9d415d208d8d3c60911580
Irvin Steve Cardenas <irvsteve@gmail.com>
2014-09-24 01:53:10 -0400

    Spam example
    Installing packages and viewing dataset

---
+++ spam.R
@@ -0,0 +1,8 @@
+install.packages("kernlab") # Install the kernlab package, take note of the quotation marks
+
+library(kernlab)            # Use the kernlab package
+
+data(spam)                  # Let's take a look at the data set
+head(spam)
+
+##################################
